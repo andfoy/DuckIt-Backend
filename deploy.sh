757a1d13dc60e3fdc357997db8c747bb712543f2
andfoy <andfoy@gmail.com>
2016-05-28 22:04:05 -0500

    Deployment script modified

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-clear
+#clear
 echo "Deploying App...\n"
 POE="main_duckit.py"
 POE2="duck_services.py"
 
 PR= echo | ps ax | grep $POE | grep -v grep | awk '{print $1}' | xargs kill > /dev/null 2>&1
 PR2= echo | ps ax | grep $POE2 | grep -v grep | awk '{print $1}' | xargs kill > /dev/null 2>&1
-
-echo "Downloading latest revision from repository...\n"
-git pull
 
 chmod 777 $POE
 chmod 777 $POE2
